9f7741591d8f85adac642cbc0279904779213129
NguyenHungKhang <104726874+NguyenHungKhang@users.noreply.github.com>
2025-04-18 10:19:25 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import SearchScore from './pages/SearchScore';
 import TopTenGroupA from './pages/TopTenGroupA';
 import Statistics from './pages/Statistics';
